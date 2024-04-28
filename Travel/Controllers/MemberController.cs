95fd3b4a03a7fbe95b2cc0953531fa1243c9b3a3
poyu <chen06115@gmail.com>
2024-04-29 00:08:57 +0800

    Login Layout

---
+++ Travel/Controllers/MemberController.cs
@@ -0,0 +1,14 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace Travel.Controllers
+{
+    public class MemberController : Controller
+    {
+        //Login 為登入畫面
+        [HttpGet]
+        public ActionResult Login()
+        {
+            return View();
+        }
+    }
+}
