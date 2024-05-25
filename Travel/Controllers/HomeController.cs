d6e1fc5f1e27799808f19b50c41d7a957f6d5caf
poyu <chen06115@gmail.com>
2024-05-26 00:14:09 +0800

    add List-Attraction function

--- Travel/Controllers/HomeController.cs
+++ Travel/Controllers/HomeController.cs
@@ -1,5 +1,7 @@
 using Microsoft.AspNetCore.Mvc;
+using Microsoft.EntityFrameworkCore;
 using System.Diagnostics;
+using Travel.Areas.Identity.Data;
 using Travel.Models;
 
 namespace Travel.Controllers
@@ -7,15 +9,18 @@ namespace Travel.Controllers
     public class HomeController : Controller
     {
         private readonly ILogger<HomeController> _logger;
+        private readonly TravelContext _context;
 
-        public HomeController(ILogger<HomeController> logger)
+        public HomeController(ILogger<HomeController> logger, TravelContext context)
         {
             _logger = logger;
+            _context = context;
         }
 
         public IActionResult Index()
         {
-            return View();
+            var attractions = _context.Attraction.ToList();
+            return View(attractions);
         }
 
         [ResponseCache(Duration = 0, Location = ResponseCacheLocation.None, NoStore = true)]
