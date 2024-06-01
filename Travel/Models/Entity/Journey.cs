f3568a8e20108c841d23d0e5de43ce439ad87004
kkx <benson08281015@gmail.com>
2024-06-01 12:19:57 +0800

    新增、修改、刪除行程

---
+++ Travel/Models/Entity/Journey.cs
@@ -0,0 +1,12 @@
+﻿namespace Travel.Models.Entity
+{
+    public class Journey
+    {
+        public Guid id { get; set; } = Guid.NewGuid();
+        public string? place { get; set; }
+        public DateTime? start_date { get; set; }
+        public DateTime? end_date { get; set; }
+        public DateTime? created_at { get; set; }
+        public DateTime? updated_at { get; set; }
+    }
+}
---
+++ Travel/Models/Entity/Reservation.cs
@@ -0,0 +1,15 @@
+﻿namespace Travel.Models.Entity
+{
+    public class Reservation
+    {
+        public int? id { get; set; }
+        public int? people { get; set; }
+        public int? status { get; set; }
+        public int? user_id { get; set; }
+        public int? Journey_id { get; set; }
+        public string? remark { get; set; }
+        public DateTime? created_at { get; set; }
+        public DateTime? updated_at { get; set; }
+
+    }
+}
