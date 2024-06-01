6138b6220fb7acd3d96b1da25f5c5fe630479ff0
Hongjouo <Lichun556206@gmail.com>
2024-06-01 12:34:12 +0800

    新增、修改、刪除Journey

--- Travel/Controllers/JourneyController.cs
+++ Travel/Controllers/JourneyController.cs
@@ -1,12 +1,80 @@
-﻿using Microsoft.AspNetCore.Mvc;
+﻿using Microsoft.AspNetCore.JsonPatch;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.Infrastructure;
+using Microsoft.EntityFrameworkCore;
+using Travel.Areas.Identity.Data;
+using Travel.Models;
+using Travel.Models.Entity;
 
 namespace Travel.Controllers
 {
     public class JourneyController : Controller
     {
-        public IActionResult Index()
+
+        private readonly TravelContext _context;
+
+        public JourneyController(TravelContext mvcDBContext)
+        {
+            this._context = mvcDBContext;
+        }
+
+        [HttpGet]
+        public IActionResult showJourney()
+        {
+            var journey = _context.Journey.ToList();
+            return View(journey);
+        }
+        [HttpGet]
+        public IActionResult addJourney()
         {
             return View();
+        }
+        [HttpPost]
+        public async Task<IActionResult> addJourney(Journey view_journey)
+        {
+            var journey = new Journey
+            {
+                id = Guid.NewGuid(),
+                place = view_journey.place,
+                start_date = view_journey.start_date,
+                end_date = view_journey.end_date,
+            };
+
+            await _context.Journey.AddAsync(journey);
+            await _context.SaveChangesAsync();
+            return RedirectToAction("showJourney","Admin");
+        }
+        [HttpPatch]
+        public async Task<IActionResult> editJourney(Journey view_journey)
+        {
+            // 檢查是否輸入資料
+            if (!ModelState.IsValid)
+            {
+                return View(view_journey);
+            }
+
+            var journey = await _context.Attraction.FindAsync(view_journey.Id);
+            if (journey != null)
+            {
+                journey.place = view_journey.place;
+                journey.start_date = view_journey.start_date;
+                journey.end_date = view_journey.end_date;
+
+                _context.Attraction.Update(journey);
+                await _context.SaveChangesAsync();
+            }
+            return RedirectToAction("showJourney", "Admin");
+        }
+        [HttpPost]
+        public async Task<IActionResult> deleteJourney(Guid id)
+        {
+            var journey = await _context.Journey.FindAsync(id);
+            if (journey != null)
+            {
+                _context.Journey.Remove(journey);
+                await _context.SaveChangesAsync();
+            }
+                return RedirectToAction("showJourney", "Admin");
         }
     }
 }
