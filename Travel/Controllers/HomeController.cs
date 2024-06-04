5762f937bfa50fec30a7dc3f5bcafbc51ebebc42
poyu <chen06115@gmail.com>
2024-06-04 11:00:19 +0800

    自動帶入預約人資料

--- Travel/Controllers/HomeController.cs
+++ Travel/Controllers/HomeController.cs
@@ -1,3 +1,4 @@
+using Microsoft.AspNetCore.Identity;
 using Microsoft.AspNetCore.Mvc;
 using Microsoft.EntityFrameworkCore;
 using System.Diagnostics;
@@ -10,25 +11,35 @@ namespace Travel.Controllers
     {
         private readonly ILogger<HomeController> _logger;
         private readonly TravelContext _context;
+        private readonly UserManager<TravelUser> _userManager;
+        private readonly SignInManager<TravelUser> _signInManager;
 
-        public HomeController(ILogger<HomeController> logger, TravelContext context)
+        public HomeController(
+            ILogger<HomeController> logger,
+            TravelContext context,
+            UserManager<TravelUser> userManager,
+            SignInManager<TravelUser> signInManager)
         {
             _logger = logger;
             _context = context;
+            _userManager = userManager;
+            _signInManager = signInManager;
         }
 
-        public IActionResult Index()
+        public async Task<IActionResult> Index()
         {
+            var user = await _userManager.GetUserAsync(User);
             var journeys = _context.Journey.ToList();
-            var viewModel = new List<JourneyAttractionViewModel>();
+            var viewModel = new List<ReservationViewModel>();
 
             foreach (var journey in journeys)
             {
                 var attractions = _context.Attraction.Where(a => a.Journey_id == journey.id).ToList();
-                viewModel.Add(new JourneyAttractionViewModel
+                viewModel.Add(new ReservationViewModel
                 {
                     Journey = journey,
-                    Attraction = attractions
+                    Attraction = attractions,
+                    User = user // 添加當前登入的使用者資料
                 });
             }
 
---
+++ Travel/ViewModel/ReservationViewModel.cs
@@ -0,0 +1,10 @@
+﻿using Travel.Areas.Identity.Data;
+using Travel.Models.Entity;
+
+public class ReservationViewModel
+{
+    public Journey Journey { get; set; }
+    public List<Attraction> Attraction { get; set; }
+    public TravelUser User { get; set; }
+    public List<Reservation> Reservations { get; set; }
+}
